0c9adaf949bc929ab315afade284139e24521f82
Stanislav <116827216+stasixlord@users.noreply.github.com>
2022-12-09 21:02:12 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = '5967973187:AAFXwa2pxM4B_0yql_vQ_z5ZoWyxeUef4wE'
+CURRENCY_API = 'd0c7d4a1ee3c0f5eac6d5bb46b670ab80d0e543ac423c2ae05f81132891f47ce'
+
+val = {
+    'евро' : 'EUR',
+    'рубль' : 'RUB',
+    'доллар' : 'USD'
+}
